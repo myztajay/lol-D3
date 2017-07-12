2a462d964f3c0ac0622284845dc9a16f75550ae3
myztajay <myztajay@hotmail.com>
2017-07-11 23:35:20 -0400

    Added MainContainer Component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import MainContainer from './components/MainContainer'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MainContainer />, document.getElementById('root'));
 registerServiceWorker();
