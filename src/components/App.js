17f979cc7c416deab2b4e426506040dce6d7b5a2
myztajay <myztajay@hotmail.com>
2017-07-13 15:10:32 -0400

    Refactored and added react-materialize library

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/App.css'
+
+
+class App extends Component {
+  
+  render(){
+    return(
+      <div>
+        <h1>This is the App.</h1>
+      </div>
+    )
+  }
+}
+
+export default App
