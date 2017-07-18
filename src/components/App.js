97e50d4fcba65af2ebef69d0cc765c74e8730e9c
myztajay <myztajay@hotmail.com>
2017-07-18 14:19:15 -0400

    Implemented browser router some dummy components

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import '../styles/App.css'
+import Landing from './Landing'
+import About from './About'
+import D3 from './D3'
 
 
 class App extends Component {
-  
   render(){
     return(
-      <div>
-        <h1>This is the App.</h1>
-      </div>
+      <BrowserRouter>
+        <div>
+          <h1>Main</h1>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route path="/about" component={About} />
+            <Route path="/:user" component={D3} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     )
   }
 }
---
+++ src/components/D3.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const D3 = () => {
+  return(
+    <div>
+      <h1>D3</h1>
+    </div>
+  )
+} 
+
+export default D3;
