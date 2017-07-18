75c7163c04de9c74a2e39937fc7bdbdc78918e2f
myztajay <myztajay@hotmail.com>
2017-07-18 17:08:44 -0400

    Created spinner component

--- src/components/D3.js
+++ src/components/D3.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
-
+import Spinner from './common/Spinner'
 
 class D3 extends Component {
   constructor(props){
     super(props)
     this.state = {
       username: props.match.params.user,
+      data: '',
       loading:true
     }
   }
+  
   render(props){
     return(
       <div>
         <h1>D3</h1>
         <h1>{this.state.username}</h1>
+        <Spinner />
       </div>
     )
   }
 } 
-
 
 export default D3;
