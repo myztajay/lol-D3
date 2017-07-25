0062d7e53943f316e80ea14a084b46e8a920421c
myztajay <myztajay@hotmail.com>
2017-07-25 11:49:05 -0400

    Added render logic to handle which graph to load based on navbar

--- src/components/GraphDisplay.js
+++ src/components/GraphDisplay.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Spinner from './common/Spinner';
-import Graph from './BarGraph';
+import BarGraph from './BarGraph';
+import RadarGraph from './RadarGraph';
+import DoughnutGraph from './DoughnutGraph'
 import GraphNavbar from './GraphNavbar';
 import '../styles/GraphDisplay.css';
 
@@ -13,7 +15,8 @@ class GraphDisplay extends Component {
       username: props.match.params.user,
       matches: [],
       userId: '',
-      loading:true
+      loading:true,
+      graph: ''
     }
   }
   
@@ -24,26 +27,34 @@ class GraphDisplay extends Component {
       this.setState({
         userId: response.data.userId, 
         matches: response.data.userMatches, 
+        graph: 'bar',
         loading: false})
     }) 
   }
   
+  handleGraphChange =(e, graph) =>{
+    e.preventDefault()
+    this.setState({
+      graph
+    })
+    console.log(this.state.graph)
+  }
+
+  
   render(props){
-    if(this.state.loading){
-      return(  
-        <Spinner />
-      )
-    }
-    else {
+    let Component = ''
+    if(this.state.loading){ Component = <Spinner /> }
+    if(this.state.graph === 'bar') Component = < BarGraph matches={ this.state.matches }/>
+    if(this.state.graph === 'doughnut') Component = < DoughnutGraph matches={ this.state.matches } />
+    if(this.state.graph === 'radar') Component = < RadarGraph matches={ this.state.matches } />
+    
     return(
       <div className='graphDisplay'>
-        <GraphNavbar />
-        <h1>D3</h1>
-        <h1>{this.state.username}</h1>
-        <Graph matches={this.state.matches} />
+        <GraphNavbar handleGraphChange={this.handleGraphChange}/>
+        {Component}
       </div>
     )
-  }
+  
   }
 } 
 
--- src/components/GraphNavbar.js
+++ src/components/GraphNavbar.js
@@ -3,10 +3,23 @@ import { Navbar, NavItem } from 'react-materialize';
 
 
 const GraphNavbar = (props) =>{
+  console.log(props)
   return(
     <Navbar className='blue darken-4'brand='logo' left>
-	   <NavItem href=''>Getting started</NavItem>
-	    <NavItem href=''>Components</NavItem>
+	  <NavItem onClick={
+      (e)=> {
+      props.handleGraphChange(e,'bar')
+    }}
+    >Bar
+    </NavItem>
+	  <NavItem onClick={
+      (e)=> {
+      props.handleGraphChange(e,'doughnut')
+    }}>Doughnut</NavItem>
+    <NavItem onClick={
+      (e)=> {
+      props.handleGraphChange(e, 'radar')
+    }}>Radar</NavItem>
     </Navbar>
   )
 }
